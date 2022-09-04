703aa5cfdc0541d4418dc3c235a143a6d1b8b8a2
JoelMendozaP <joelmendozapaucara@gmail.com>
2022-09-04 17:41:55 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su nombre:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hola " + name);
+            Console
+                .WriteLine("Son las " + DateTime.Now.ToString("hh:mm:ss tt"));
+        }
+    }
+}
